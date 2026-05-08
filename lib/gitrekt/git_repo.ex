e47dae4f48e0fd78d96ccc143f22c7f6f113c049
Sergey Moiseev <moiseev.sergey@gmail.com>
2026-05-08 16:28:20 +0300

    Add pre_push callback to GitRepo protocol
          Allows applications to validate ref updates before they are applied (pre-push hook).
          Called between push_pack and push_cmds in receive_pack protocol handler.
          Returns :ok to allow, {:error, reason} to reject the push.

--- lib/gitrekt/git_repo.ex
+++ lib/gitrekt/git_repo.ex
@@ -15,7 +15,15 @@ defprotocol GitRekt.GitRepo do
   def get_agent(repo)
 
   @doc """
-  Pushes the
+  Validates ref updates before they are applied (pre-push hook).
+  Called before push_cmds to allow custom validation logic.
+  Return :ok to allow, {:error, reason} to reject the push.
+  """
+  @spec pre_push(t, [ReceivePack.cmd]) :: :ok | {:error, binary}
+  def pre_push(repo, cmds)
+
+  @doc """
+  Executes after refs have been updated (post-push hook).
   """
   @fallback_to_any true
   @spec push(t, [ReceivePack.cmd]) :: {:ok, t} | {:error, term}
