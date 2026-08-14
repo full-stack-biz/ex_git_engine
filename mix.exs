946d4ab095523fd3b29c59ef12bf4f808d94dc9a
Sergey Moiseev <moiseev.sergey@gmail.com>
2026-08-14 19:17:41 +0300

    Add CHANGELOG and bump version to 0.9.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExGitEngine.Mixfile do
   def project do
     [
       app: :ex_git_engine,
-      version: "0.9.1",
+      version: "0.9.2",
       build_path: "_build",
       config_path: "config/config.exs",
       deps_path: "deps",
